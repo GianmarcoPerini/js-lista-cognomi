653a5e865281b9c81689e0d35f6fa596a823580a
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-26 17:33:02 +0200

    rev_1

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const capitalizzazione = tuoCognome.charAt(0).toUpperCase() + tuoCognome.slice(1
 listaCognomi.push(capitalizzazione)
 console.log(listaCognomi.sort());
 console.log(listaCognomi.indexOf(capitalizzazione) + 1);
+
+for (let i = 0; i < listaCognomi.length; i++){
+    let storico = document.getElementById("lista").innerHTML;
+    document.getElementById("lista").innerHTML = storico + "<li>" + listaCognomi[i] + "</li>"
+
+}
+
+ducument.getElementById("posizione").innerHTML = "ciao"
